e0542feee032b3ea377f5a67ce38fd787149b701
Greg Garville Jr <gjgarvil@gmail.comgit config --global user.name Greg>
2017-07-25 17:14:45 -0400

    styling finished, working on js

--- directory-app.js
+++ directory-app.js
@@ -9,3 +9,11 @@ app.use(express.static('public'))
 app.engine('mustache', mustacheExpress())
 app.set('views', './views')
 app.set('view engine', 'mustache')
+
+app.get('/', (req, res) => {
+	res.render('index', data)
+})
+
+app.listen(7777, function() {
+	console.log('Looking good Billy Ray!!!')
+})
