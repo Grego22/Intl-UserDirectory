1dfa007edea3399bdbbb2e0aa201b0042f79fbe9
Greg Garville Jr <gjgarvil@gmail.comgit config --global user.name Greg>
2017-07-26 14:22:55 -0400

    first part of css finished

--- directory-app.js
+++ directory-app.js
@@ -1,19 +1,34 @@
 const express = require('express')
 const app = express()
-const data = require('./data.js')
+const data = require('./data')
+
+//mark would use console.log data hear to make sure we have it!
+console.log(data);
 
 const mustacheExpress = require('mustache-express')
 
+//teach our app to use public for all public files
 app.use(express.static('public'))
 
+//teach our app to use mustache engine for rendering template
 app.engine('mustache', mustacheExpress())
+
+//teach our app where the views (templates) live
 app.set('views', './views')
+
+//teach our app to use mustache engine for our template
 app.set('view engine', 'mustache')
 
-app.get('/', (req, res) => {
-	res.render('index', data)
+app.get('/', (request, response) => {
+  //sending content to the browser from javascript
+	response.render('index', data)
 })
-
+/*
+app.get('users/:id', (request, response) => {
+	response.render('users', data)
+  const requestId = request.params.id
+})
+*/
 app.listen(7777, function() {
 	console.log('Looking good Billy Ray!!!')
 })
